d9ddb9ab72171b041a3f3e2ca274b0a49033b6dd
idenginfotech <divyasolanki016@gmail.com>
2021-07-17 16:36:19 +0530

    add file

---
+++ admin/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>beauty_product</title>
+	<link rel="stylesheet" type="text/css" href="../css/style.css">
+</head>
+<body>
+<div class="menu">
+	<div class="wrapper">
+		<ul>
+			<li><a href="#">HOME</a></li>
+			<li><a href="#">ABOUT OUR PRODUCT</a></li>
+		    <li><a href="#">ADMIN</a></li>
+		    <li><a href="#">PRODUCTS</a></li>
+		    <li><a href="#">SHOP</a></li>
+		    <li><a href="#">CART</a></li>
+		</ul>
+		
+		
+		
+
+	</div>
+</div>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>beauty_product</title>
+	 <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+	<link rel="stylesheet" type="text/css" href="./css/style.css">
+</head>
+<body>
+<div class="menu">
+	<div class="wrapper">
+		<ul>
+			<li><a href="#">HOME</a></li>
+			<li><a href="#">ABOUT OUR PRODUCT</a></li>
+		    <li><a href="#">ADMIN</a></li>
+		    <li><a href="#">PRODUCTS</a></li>
+		    <li><a href="#">SHOP</a></li>
+		    <li><a href="#">CART</a></li>
+		</ul>
+		
+		
+		
+
+	</div>
+</div>
+<hr>
+
+<div class="main">
+	<div class="wrapper">
+
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-3"><a href="#"><img src="./image/olay.jpg"></a></br> 
+                 <p> <b>OLAY -A GIFT FROM A MAN TO HIS WIFE</b></br>
+                     The original “pink” Beauty Fluid is created by South African chemist Graham Wulff, inspired by his wife and other women who want deep hydration without the thick heavy creams of the day.</p>
+
+				</div>
+				<div class="col-sm-3"><a href="#"><img src="./image/o3.jpg"></a>
+				<p> <b>O3 PRODUCTS</b></br>
+                     O3+ brings to you award winning professional skin solutions with the highest quality standards that cater to all your skin concerns such as Acne, Pigmentation, Wrinkles, Dark circles, Dullness etc. Choose among our best selling Bridal Facial Kits to our Cult favourite Dtan Pack and Vitamin C Serum, our range will leave your skin with a long lasting glow and radiance. Our formulas combine the research & development of dermatologist of over 30 Years backed by the incredible word of mouth and miraculous results achieved by Salons and Beauty Professionals.</p>
+	
+
+				</div>
+				<div class="col-sm-3"><a href="#"><img src="./image/nyka.jpg"></a>
+					<p> <b>NYKA-BEAUTY</b></br>
+                     Our #LookOfTheMonth award continues. We will be choosing the best looks to feature on the Nykaa Army account at the end of the month. All you gotta do is, use #LookOfTheMonth in your post to participate and win. Get going girls </p>
+
+				</div>
+				<div class="col-sm-3"><a href="#"><img src="./image/drema.jpg"></a>
+					<p> <b>DREMA-SKIN CARE</b></br>
+                     This medication is used as a moisturizer to treat or prevent dry, rough, scaly, itchy skin and minor skin irritations (e.g., diaper rash, skin burns from radiation therapy). Emollients are substances that soften and moisturize the skin and decrease itching and flaking. Some products (e.g., zinc oxide, white petrolatum) are used mostly to protect the skin against irritation (e.g., from wetness)S </p>
+
+
+
+				</div>
+			</div>
+
+		</div>
+		
+	</div>
+
+</div>
+
+<div class="bottom">
+	<div class="wrapper">
+
+		<div class="row">
+		<div class="col-sm-4">
+			<ul>
+				<li><strong>PRODUCT</strong></li>
+				<li><a href="">About</a></li>
+				<li><a href="">Contact Us</a></li>
+			</ul>
+		</div>
+		<div class="col-sm-4">
+			<ul>
+				<li><strong>NEED HELP?</strong></li>
+				<li>Email us</li>
+				<li>divyasolanki016@gmail.com</li>
+				<li>call us</li>
+				<li>3644765588</li>
+			</ul>
+
+		 </div>
+		<div class="col-sm-4">
+			<ul>
+				<li><strong>CUSTOMER CARE</strong></li>
+				<li><a href="">Terms and conditions</a></li>
+				<li><a href="">Privacy Policy</a></li>
+				<li><a href="">FAQs</a></li>
+				
+			</ul>
+
+
+		</div>
+	</div>
+		
+
+	</div>
+	
+
+
+</div>
+
+<div class="footer">
+	<div class="wrapper">
+		All reights reserved by divya solanki, <a href="">beauty product.</a> 
+		
+
+
+	</div>
+
+</div>
+
+</body>
+</html>
